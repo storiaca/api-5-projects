9604faed04fbe369c5736cc442ee7e4c6df79035
Aleksandar Ristic <storiaca@gmail.com>
2022-03-01 17:56:11 +0100

    Solve sudoku, create html js and css file, start

---
+++ sudoku-solver/app.js
@@ -0,0 +1,21 @@
+const puzzleBoard = document.querySelector("#puzzle");
+const solveButton = document.querySelector("#solve-button");
+const squares = 81;
+const submission = [];
+
+for (let i = 0; i < squares; i++) {
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "number");
+  inputElement.setAttribute("min", 0);
+  inputElement.setAttribute("max", 9);
+
+  puzzleBoard.appendChild(inputElement);
+}
+
+const joinValues = () => {
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    if (input.value) {
+    }
+  });
+};
