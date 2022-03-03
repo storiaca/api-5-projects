20e3fabb4d36b43b8bcf201c2e7e2d15d9092332
Aleksandar Ristic <storiaca@gmail.com>
2022-03-03 17:56:21 +0100

    Add rapid api code for sudoku

--- sudoku-solver/app.js
+++ sudoku-solver/app.js
@@ -6,7 +6,7 @@ const submission = [];
 for (let i = 0; i < squares; i++) {
   const inputElement = document.createElement("input");
   inputElement.setAttribute("type", "number");
-  inputElement.setAttribute("min", 0);
+  inputElement.setAttribute("min", 1);
   inputElement.setAttribute("max", 9);
 
   puzzleBoard.appendChild(inputElement);
@@ -16,6 +16,43 @@ const joinValues = () => {
   const inputs = document.querySelectorAll("input");
   inputs.forEach((input) => {
     if (input.value) {
+      submission.push(input.value);
+    } else {
+      submission.push(".");
     }
   });
+  //console.log(submission);
 };
+
+const populateValues = (response) => {
+  const inputs = document.querySelectorAll("input");
+};
+
+const solve = () => {
+  joinValues();
+  const data = submission.join("");
+  console.log("data", data);
+  const options = {
+    method: "POST",
+    url: "https://solve-sudoku.p.rapidapi.com/",
+    headers: {
+      "content-type": "application/json",
+      "x-rapidapi-host": "solve-sudoku.p.rapidapi.com",
+      "x-rapidapi-key": "",
+    },
+    data: {
+      puzzle: data,
+    },
+  };
+
+  axios
+    .request(options)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+solveButton.addEventListener("click", solve);
